b58a07d01200ff370377dfb409e6ca412086c295
Archit Chandrakar <bankofhawkgatsu@gmail.com>
2024-04-07 00:44:59 +0530

    Minor progress

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
+	"time"
 
 	"github.com/xmp-er/rcrd/helpers"
 )
 
 func main() {
+
 	var cmd *exec.Cmd
 
-	os := helpers.GetOS()
+	operating_system := helpers.GetOS()
 
-	switch os {
-	case "darwin":
-		cmd = exec.Command("ffmpeg", "-f", "avfoundation", "-i", "1:none", "-f", "avfoundation", "-i", ":1", "-c:v", "libx264", "-c:a", "aac", "output.mp4")
-	case "windows":
-		cmd = exec.Command("ffmpeg", "-f", "gdigrab", "-framerate", "30", "-i", "desktop", "-f", "dshow", "-i", "audio=virtual-audio-capturer", "-c:v", "libx264", "-c:a", "aac", "output.mp4")
-	case "linux":
-		cmd = exec.Command("ffmpeg", "-f", "x11grab", "-framerate", "25", "-i", ":0.0", "-f", "alsa", "-i", "default", "-c:v", "libx264", "-c:a", "aac", "output.mp4")
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory: ", err)
+		return
 	}
+
+	outputFile := "output.mp4"
+	outputPath := filepath.Join(currentDir, outputFile)
+
+	switch operating_system {
+	case "darwin":
+		cmd = exec.Command("ffmpeg", "-f", "avfoundation", "-i", "1:none", "-f", "avfoundation", "-i", ":1", "-c:v", "libx264", "-c:a", "aac", outputPath)
+	case "windows":
+		cmd = exec.Command("ffmpeg", "-f", "gdigrab", "-framerate", "30", "-i", "desktop", "-f", "dshow", "-i", "audio=virtual-audio-capturer", "-c:v", "libx264", "-c:a", "aac", outputPath)
+	case "linux":
+		cmd = exec.Command("ffmpeg", "-f", "x11grab", "-framerate", "25", "-i", ":0.0", "-f", "alsa", "-i", "default", "-c:v", "libx264", "-c:a", "aac", outputPath)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error while starting the command ", err)
+	}
+	fmt.Println("recording started")
+	time.Sleep(5 * time.Second)
+	err = cmd.Process.Signal(syscall.SIGINT)
+	if err != nil {
+		fmt.Println("Error while stopping the process ", err)
+	}
+
 }
